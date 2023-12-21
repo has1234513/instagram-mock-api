2955be3d18cc1309d9dbfd4797a0b1da41f6360a
has1234513 <hashaydar29@gmail.com>
2023-12-22 00:55:13 +0200

    change

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
